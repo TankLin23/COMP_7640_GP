01a2df6f401deae81e958dc9c356ee539f8f581e
Tank <liangyuanlin775@gmail.com>
2024-03-15 16:39:55 +0800

    initial project

---
+++ src/VendorFrame.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import javax.swing.GroupLayout;
+/*
+ * Created by JFormDesigner on Fri Mar 15 16:38:41 CST 2024
+ */
+
+
+
+/**
+ * @author 10468
+ */
+public class VendorFrame extends JFrame {
+    public VendorFrame() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+
+        //======== this ========
+        var contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 400, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 300, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
