e1bab40e3f4547a1c6c78ac0440411d9cf5682c4
Tank <liangyuanlin775@gmail.com>
2024-03-21 18:55:32 +0800

    final version

--- src/scriptnew.sql
+++ src/scriptnew.sql
@@ -1,3 +1,6 @@
+
+create database if not exists vendor_customer;
+
 create table customer
 (
     customer_id      int auto_increment
@@ -43,5 +46,20 @@ create table vendor
     geographical_presence varchar(255)  null,
     password              varchar(10)   not null
 );
+
+insert into customer values (12345,'23569874','6','123456');
+insert into `order` values (1,12345,'unshipped','HKBU','12345','1','1999','Phone','0');
+insert into `order` values (2,12345,'shipped','HKBU','12345','1','1999','Phone','1');
+insert into `order` values (4,12345,'unshipped','HKBU','12345','1','1999','Phone','0');
+insert into `order` values (5,12345,'unshipped','HKU','12359','2','6000','iPad','0');
+insert into `order` values (6,12345,'unshipped','HKU','12359','1','1999','iPad','0');
+insert into product values (1,'12345','Phone','2000','digital products','cost performance','fashion','1000');
+insert into product values (1,'12359','iPad','3000','digital products','cost performance','fashion','996');
+insert into vendor values (12345,'HKBU',1.1,'HongKong','123456');
+insert into vendor values (12346,'zhongshan',1.0,'HongKong','12345');
+insert into vendor values (12348,'Tsinghua',1.1,'HongKong','123456');
+insert into vendor values (12359,'cityU',1.1,'HongKong','123456');
+insert into vendor values (12347,'polyU',1.1,'HongKong','123456');
+
 
 
